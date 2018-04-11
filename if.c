ee0a707115aa1e6b3450e8a6e41cf9aed276dd31
ITblueboy <18309208067@163.com>
2018-04-11 23:37:58 +0800

    选择结构

---
+++ if.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	int a = 0;
+	int b = 2;
+	if (a == 1)
+		if (b == 2)
+			printf("hehe\n");
+	else
+		printf("haha\n");
+	system("pause");
+	return 0;
+}
