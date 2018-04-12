906d4c0c7076eb735b61d6b9af0d2ea4e682906c
ITblueboy <18309208067@163.com>
2018-04-12 23:58:13 +0800

    循环

---
+++ while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<windows.h>
+
+//int main()
+//{
+//	int i = 1;
+//	while (i<=10)
+//	{
+//		i++;
+//		if (i == 5)
+//		continue;
+//		printf("%d", i);
+//		
+//	}
+//	system("pause");
+//	return 0;
+//}
