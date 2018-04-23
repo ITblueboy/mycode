d6eb73a7ba4684cbbf956a8bec780eaf15cb3693
ITblueboy <18309208067@163.com>
2018-04-23 23:54:11 +0800

    闰年

---
+++ year.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<Windows.h>
+
+int main()
+{
+	int year = 1000;
+	for (year=1000; year <= 2000; year++)
+	{
+		if (((year % 4 == 0) && (year % 100 != 0))||(year%400==0))
+		{
+			printf("%d\t", year);
+		}
+	}
+	system("pause");
+	return 0;
+}
