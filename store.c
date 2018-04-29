f7154af47263cce445a0b5ac8f9da0a2b7ac83f4
ITblueboy <18309208067@163.com>
2018-04-30 00:44:01 +0800

    数据存储

---
+++ store.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+  char a=-1;
+  signed char b=-1;
+  unsigned char c=-1;
+  char d=-128;
+  char e=128;
+  printf("a=%d,b=%d,c=%d,d=%u,e=%u\n",a,b,c,d,e);
+  return 0;
+}
