0fe7f068e4b37b1abf02bf37e932e6df61a61b0d
ITblueboy <18309208067@163.com>
2018-05-16 20:03:47 +0800

    进程替换

---
+++ exec.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+  char* const argv[]={"ps","-ef",NULL};
+  char* const envp[]={"PATH=/bin:/usr/bin","TERM=console",NULL};
+  execl("/bin/ps","ps","-ef",NULL);
+  execlp("ps","ps","-ef",NULL);
+  execle("ps","ps","-ef",NULL,envp);
+  execv("/bin/ps",argv);
+  execvp("ps",argv);
+  execve("/bin/ps",argv,envp);
+  exit(0);
+}
